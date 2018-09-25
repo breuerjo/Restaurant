0a9416b663e769f4c8a169ac3ded3b94edea988b
Jonas <Jonas@DESKTOP-TRNQUNV>
2018-09-25 20:33:05 +0200

    Gestaltung Login-Fenster für Mitarbeiter 0,5h

--- Restaurant/src/EinloggenMitarbeiter.php
+++ Restaurant/src/EinloggenMitarbeiter.php
@@ -1,3 +1,23 @@
+<!DOCTYPE>
+<html>
+
+<head>
+
+
+  <title>Mittarbeiter-Einloggen</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="..\css\style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <!--Diese beiden sind neu-->
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+ <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">-->
+
+</head>
+
+
 <?php 
 
     function SignIn() { 
@@ -14,9 +34,15 @@
             if(!empty($row['mitarbeiter_benutzername']) AND !empty($row['mitarbeiter_passwort'])) { 
                 $_SESSION['mitarbeiter_benutzername'] = $row['mitarbeiter_benutzername']; 
                 
+                /*
                 echo '<p>SUCCESSFULLY LOGIN TO USER PROFILE PAGE...</p><br>';
                 echo $_SESSION["mitarbeiter_benutzername"];
                 echo '<button class="btn btn-default"><a href="index-Mitarbeiter.php">Weiter zur Startseite</a></button>';
+                */
+                
+                echo '<div class="w3-row-padding w3-padding-16 w3-center w3-margin-top">Login erfolgreich mit dem Benutzernamen: ',$_SESSION["mitarbeiter_benutzername"],'<br>
+                <br><button class="w3-button w3-blue"><a href="index-Mitarbeiter.php">Weiter zur Startseite</a></button></div>';
+            } 
                 //COOKIE SETZEN!
                 //setcookie("mitarbeiter_benutzername", $row['mitarbeiter_benutzername'], time() + (86400 * 1), "/"); // 86400 = 1 day
                 //echo '<p>Benutzername ist:  ', $_COOKIE["mitarbeiter_benutzername"],' </p>';
@@ -28,17 +54,31 @@
                       
                 */
                 
-            } 
+             
             else { 
-                echo "Falscher Benutzername oder Passwort <br>"; 
-                echo '<button class="btn btn-default"><a href="Mitarbeiter-Login.php">Zurück zum Login</a></button>';
+                echo '<div class="w3-row-padding w3-padding-16 w3-center w3-margin-top">Falscher Benutzername oder Passwort <br> 
+                <br><button class="w3-button w3-red"><a href="Mitarbeiter-Login.php">Zur&uuml;ck zum Login</a></button></div>';
             } 
         }
         
-        
-        
-    if(isset($_POST['login'])) { 
-        SignIn(); 
-    }
 
 ?>
+
+
+
+<body>
+	<?php 
+	if(isset($_POST['login'])) {
+	    SignIn();
+	}
+	 ?>
+</body>
+
+<footer class="container-fluid md-12 text center navbar-fixed-bottom">
+  	<div class="col-md-4 navbar-text pull-left"><p><a href="Impressum.php"><b>Impressum</b></a></p></div> 
+  	<div class="col-md-4 navbar-text pull-right"><p><a href="Impressum.php"><b>Datenschutz</b></a></p></div>
+</footer>
+</html>
+
+
+
