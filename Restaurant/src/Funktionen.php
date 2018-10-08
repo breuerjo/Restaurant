b6b2df1202e3bd591decd6ff9ce670fd097751ef
Jonas <Jonas@192.168.0.81>
2018-10-08 23:39:04 +0200

    MERGE MIT MEINEN ALTEN ÄNDERUNGEN

--- Restaurant/src/Funktionen.php
+++ Restaurant/src/Funktionen.php
@@ -1,52 +1,53 @@
 <?php
 
-    function getRestaurantName() {
-        $pdo = new PDO('mysql:host=localhost;dbname=restaurant_db;charset=utf8', 'root', '');
-        $statement = $pdo->prepare("SELECT restaurant_name FROM restaurant WHERE restaurant_id = 1");
-        $statement->execute(array());
-        $row = $statement->fetch();
-        $name = $row[0];
-        return $name;
-    }
+function getRestaurantName() {
+    $pdo = new PDO('mysql:host=localhost;dbname=restaurant_db;charset=utf8', 'root', '');
+    $statement = $pdo->prepare("SELECT restaurant_name FROM restaurant WHERE restaurant_id = 1");
+    $statement->execute(array());
+    $row = $statement->fetch();
+    $name = $row[0];
+    return $name;
+}
+
+function dbQuery($query, $array = array()){
+    $pdo = new PDO('mysql:host=localhost;dbname=restaurant_db;charset=utf8', 'root', '');
+    $statement = $pdo->prepare($query);
+    $statement->execute($array);
+    return $statement->fetchAll();
+}
+
+function getPDO(){
+    return new PDO('mysql:host=localhost;dbname=restaurant_db;charset=utf8', 'root', '');
+}
+
+function urlParameter($name){
+    return isset($_GET[$name]) ? $_GET[$name] : "";
+}
+
+function postParameter($name){
+    return isset($_POST[$name]) ? $_POST[$name] : "";
+}
+
+function getKundenId(){
+    //         if( isset($_GET['kunde'])){
+    //             return $_GET['kunde'];
+    //         }
+    return $_COOKIE['gast'];
+    //return $_SESSION['gast'];
+}
+
+function getNavbar(){
+    include_once 'Navbar.php';
     
-    function dbQuery($query, $array = array()){
-        $pdo = new PDO('mysql:host=localhost;dbname=restaurant_db;charset=utf8', 'root', '');
-        $statement = $pdo->prepare($query);
-        $statement->execute($array);
-        return $statement->fetchAll();
+    if(isset($_SESSION['mitarbeiter_benutzername'])) {
+        getNavbarMitarbeiter();
     }
-    
-    function getPDO(){
-        return new PDO('mysql:host=localhost;dbname=restaurant_db;charset=utf8', 'root', '');
+    else{
+        getNavbarKunde(getKundenId());
     }
-        
-    function urlParameter($name){
-        return isset($_GET[$name]) ? $_GET[$name] : "";
-    }
-    
-    function postParameter($name){
-        return isset($_POST[$name]) ? $_POST[$name] : "";
-    }
-    
-    function getKundenId(){
-//         if( isset($_GET['kunde'])){
-//             return $_GET['kunde'];
-//         }
-        return $_COOKIE['gast'];
-        //return $_SESSION['gast'];
-    }
-    
-    function getNavbar(){
-        include_once 'Navbar.php';
-        
-        if(isset($_SESSION['mitarbeiter_benutzername'])) {
-            getNavbarMitarbeiter();
-        }
-        else{
-            getNavbarKunde(getKundenId());
-        }
-    }
+}
 
 
 ?>
+
 
