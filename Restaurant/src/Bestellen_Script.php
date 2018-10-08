eece90cff52a74e41f2e1c8faa04e32e7dee7401
Felix <Felix@192.168.0.45>
2018-10-08 23:18:44 +0200

    formnatting

--- Restaurant/src/Bestellen_Script.php
+++ Restaurant/src/Bestellen_Script.php
@@ -1,15 +1,17 @@
 <?php
-$gericht = $_POST['gericht'];     //die gericht id
+$gericht = $_POST['gericht']; // die gericht id
 $kunde = $_POST['kunde'];
 
-//ich brauche ein gericht und nen kunden -> packt des zeug in den warenkorb
+// ich brauche ein gericht und nen kunden -> packt des zeug in den warenkorb
 
 $pdo = new PDO('mysql:host=localhost;dbname=restaurant_db;charset=utf8', 'root', '');
 
-
 $statement = $pdo->prepare("INSERT INTO warenkorb (gericht_id, gast_id) values (:gericht, :kunde)");
-     
-$statement->execute(array(':gericht' => $gericht, ':kunde' => $kunde)); 
+
+$statement->execute(array(
+    ':gericht' => $gericht,
+    ':kunde' => $kunde
+));
 
 echo $gericht, " bestellt von kunde  ", $kunde;
 ?>
--- Restaurant/src/Bestellung_loeschen.php
+++ Restaurant/src/Bestellung_loeschen.php
@@ -1,5 +1,7 @@
 <?php
-    include 'Funktionen.php';
-    $bestellung_id = urlParameter('bestellung_id');
-    dbQuery("DELETE FROM `bestellung` WHERE `bestellung_id` =:bestellung_id", array(':bestellung_id' => $bestellung_id));
+include 'Funktionen.php';
+$bestellung_id = urlParameter('bestellung_id');
+dbQuery("DELETE FROM `bestellung` WHERE `bestellung_id` =:bestellung_id", array(
+    ':bestellung_id' => $bestellung_id
+));
 ?>
