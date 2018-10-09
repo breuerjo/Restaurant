e55361f2cf85e91ad104964e7d4bfca384208c10
D070423 <D070423@WDFN34222730A.emea.global.corp.sap>
2018-10-09 10:32:02 +0200

    Bild in Navbar

--- Restaurant/src/Navbar.php
+++ Restaurant/src/Navbar.php
@@ -9,8 +9,10 @@
         <span id="Navbar1"class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
-      </button>
+      </button>      
+      <a href="Startseite.php"><img alt="Logo" src="..\img\Logo.jpg" width="70px" height="48px"></a>
       <a id="restaurant-name" class="navbar-brand" href="Startseite.php"><b><?php echo getRestaurantName(); ?></b></a>
+
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav navbar-right">
@@ -48,13 +50,13 @@ function getNavbarMitarbeiter(){
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
+      <a href="Startseite.php"><img alt="Logo" src="..\img\Logo.jpg" width="75px" height="54px"></a>      
       <a id="restaurant-name" class="navbar-brand" href="Startseite.php"><b><?php echo getRestaurantName(); ?></b></a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav navbar-right white">
         <li><a id="Navbar_HomeM" href="Startseite.php"><b>HOME</b></a></li>
         <li><a id="Navbar_SpeisenM"href="Speisekarte.php"><b>Unsere Speisen</b></a></li>
-        <!--  <li><a href="Buffet.php">Buffet/Preise</a></li>-->
         <li><a id="Navbar_BestellungenM" href="Bestellungen.php"><b>Bestellungen</b></a></li>
         <li><a id="Navbar_Mitarbeiterübersicht" href="Mitarbeiter&uuml;bersicht.php"><b>Mitarbeiter&uuml;bersicht</b></a></li>
         <li>
